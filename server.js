f39d19e90385e8c2fa4537dee65c7e09d596a3de
Devansh Rajput <devanshrajput032006@gmail.com>
2025-08-14 15:35:12 +0530

    orign / removed

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ require('dotenv').config();
 
 const app = express();
 app.use(cors({
-  origin: ['https://devansh-llm.vercel.app', 'http://localhost:5173/'],
+  origin: ['https://devansh-llm.vercel.app', 'http://localhost:5173'],
   credentials: true
 }));
 app.use(express.json({ limit: '2mb' }));
